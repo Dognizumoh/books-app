eb32e19bb33cec168efc048041d795a194a5d5d4
Dognizumoh <idafolo16@gmail.com>
2022-04-19 11:20:07 +0000

    new codes

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='page-footer'>
+    <div className='footer-copyright'>
+    <div className='container'> &copy; Books 2022
+    <a href='#!' className='grey-text text-lighten-4 right'>Premest</a>
+    </div>
+    </div>
+
+
+
+    </footer>
+  )
+}
+
+export default Footer
